e6b0e2e2e72c87572ac5e3f38007eb6f45fd61e4
aleksandr.rakov <aleksandr.rakov@gmail.com>
2016-12-22 07:21:01 +0300

    dns_rpz_support

--- load_file_to_dns.sh
+++ load_file_to_dns.sh
@@ -4,6 +4,6 @@ SERVER=$1
 PORT=$2
 FNAME=$3
 
-scp -P $PORT $FNAME $SERVER:/home/doggy/dns
+scp -oStrictHostKeyChecking=no -P $PORT $FNAME $SERVER:/home/doggy/dns
 
-ssh -p $PORT $SERVER "sudo cp /home/doggy/dns /etc/bind/db.rpz.zone; sudo /dns-restart.sh"
+ssh -oStrictHostKeyChecking=no -p $PORT $SERVER "sudo cp /home/doggy/dns /etc/bind/db.rpz.zone; sudo /dns-restart.sh"
